c9e86f8fae279ea2a115bdb166ef225ceb196df4
iamajaynayak <63513168+iamajaynayak@users.noreply.github.com>
2021-01-22 15:13:20 +0530

    full player functionality added

--- script.js
+++ script.js
@@ -23,10 +23,28 @@ function updatePlayIcon() {
 }
 
 // Update progress and timestamp
-function updateProgress() {}
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // set minutes
+  let min = Math.floor(video.currentTime / 60);
+  if (min < 10) {
+    min = "0" + String(min);
+  }
+
+  // set seconds
+  let sec = Math.floor(video.currentTime % 60);
+  if (sec < 10) {
+    sec = "0" + String(sec);
+  }
+
+  timestamp.innerHTML = `${min}:${sec}`;
+}
 
 //set video time to progress
-function setVideoProgress() {}
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
 
 //stop video
 function stopVideo() {
